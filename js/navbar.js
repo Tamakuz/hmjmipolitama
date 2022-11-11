3e0961f9b86acc6e99a41fc1b0a21cdb6d8c8ea8
Tamakuz <jovangithub@gmail.com>
2022-11-11 09:42:35 +0700

    Project HMJ MI

---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop || document.documentElement.scrollTop > 130){
+        document.getElementById('navbar').style.backgroundColor = 'white';
+        document.getElementById('navbar').style.height = "60px";
+        document.getElementById('navbar').style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0.75)';
+        document.getElementById('navbar').style.transition = 'ease-in-out';
+        document.getElementById('navbar').style.transitionDuration = '0.3s';
+        document.getElementById('title-brand').style.color = 'black' ;
+        document.getElementById('list-home').style.color = 'white';
+        document.getElementById('list-about').style.color = 'black';
+        document.getElementById('list-visi-misi').style.color = 'black';
+        document.getElementById('list-anggota').style.color = 'black';
+        document.getElementById('list-contact').style.color = 'black';
+    } else {
+        document.getElementById('navbar').style.backgroundColor = 'transparent';
+        document.getElementById('navbar').style.boxShadow = 'none';
+        document.getElementById('title-brand').style.color = 'white' ;
+        document.getElementById('list-home').style.color = 'white';
+        document.getElementById('list-about').style.color = 'white';
+        document.getElementById('list-visi-misi').style.color = 'white';
+        document.getElementById('list-anggota').style.color = 'white';
+        document.getElementById('list-contact').style.color = 'white';
+    }
+}
