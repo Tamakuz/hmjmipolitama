dd5cb930fe83ab1e9e27a8703eb136853a2ce11f
Tamakuz <jovangithub@gmail.com>
2022-11-28 09:58:41 +0700

    fix nav responsive

--- js/navToggle.js
+++ js/navToggle.js
@@ -1,7 +1,6 @@
 humberger = document.querySelector(".humberger");
 menu = document.querySelector(".navbar ul");
 
-
 humberger.onclick = function() {
     let active = menu.classList.toggle("active");
     if (active) {
@@ -16,7 +15,7 @@ humberger.onclick = function() {
         document.getElementById('navbar').style.transition = 'ease-in-out';
         document.getElementById('navbar').style.transitionDuration = '.5s';
         document.getElementById('title-brand').style.color = 'black' ;
-        document.querySelector('.humberger').style.color = 'black';
+        humberger.style.color = 'black';
         window.onscroll = function() {
             if (document.body.scrollTop || document.documentElement.scrollTop > 0){
                 document.getElementById('list-about').style.color = 'black';
@@ -28,16 +27,8 @@ humberger.onclick = function() {
                 document.getElementById('navbar').style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0.75)';
                 document.getElementById('navbar').style.transition = 'ease-in-out';
                 document.getElementById('navbar').style.transitionDuration = '0.3s';
-                document.getElementById('title-brand').style.color = 'black' ;
-                document.querySelector('.humberger').style.color = 'black';
             }
         };
-    } else {
-        window.onscroll = function() {scrollFunction()}
-        document.getElementById('navbar').style.backgroundColor = 'transparent';
-        document.getElementById('title-brand').style.color = 'white' ;
-        document.querySelector('.humberger').style.color = 'white';
-        document.getElementById('navbar').style.boxShadow = '0px 1px 3px 0px rgba(0,0,0,0)';
     };
 };
 
